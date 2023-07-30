c9af7f394c03fea7bd8b6853cae13dc79769510b
bible <mongry999@gmail.com>
2023-07-31 07:16:04 +0900

    [feat]지도 커스텀 마커 모바일용 생성

--- src/Components/SearchPage/Contents/CustomMapMarker.tsx
+++ src/Components/SearchPage/Contents/CustomMapMarker.tsx
@@ -1,7 +1,20 @@
 import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 
-const CustomMapMarker = ({ title }: { title: string }) => {
+const CustomMapMarker = ({
+  title,
+  windowWidth,
+}: {
+  title: string;
+  windowWidth: number;
+}) => {
+  const mobileContentArray = [
+    '<div style="margin: 0; display: table; padding: 0.5rem; table-layout: auto; border-radius: 2.3rem; border: 0.2rem solid var(--color--darkgreen); background: white; cursor: pointer; position: relative; z-index: 2">',
+    '<div style="display: table-cell; display: inline-block; width: 2.5rem; height: 2.5rem; background-image: url(Images/markerIcon.svg); background-size: cover; background-position: center; background-repeat: no-repeat;"></div>',
+    '<span style="position: absolute; border-style: solid; border-width: 1rem 1rem 0 1rem; border-color: #ffffff transparent; display: block; width: 0; z-index: 1; top: 3.1rem; left: 0.75rem;"></span>',
+    '<span style="position: absolute; border-style: solid; border-width: 1rem 1rem 0 1rem; border-color: var(--color--darkgreen) transparent; display: block; width: 0; z-index: 0; top: 3.35rem; left: 0.75rem;"></span>',
+    '</div>',
+  ];
   const contentArray = [
     '<div style="margin: 0; display: table; padding: 0.5rem; table-layout: auto; border-radius: 2.3rem; border: 0.2rem solid var(--color--darkgreen); background: white; cursor: pointer; position: relative; z-index: 2">',
     '<div style="display: table-cell; display: inline-block; width: 4rem; height: 4rem; background-image: url(Images/markerIcon.svg); background-size: cover; background-position: center; background-repeat: no-repeat;"></div>',
@@ -12,6 +25,8 @@ const CustomMapMarker = ({ title }: { title: string }) => {
     '<span style="position: absolute; border-style: solid; border-width: 1.2rem 1rem 0 1rem; border-color: var(--color--darkgreen) transparent; display: block; width: 0; z-index: 0; top: 5.05rem; left: 1.4rem;"></span>',
     '</div>',
   ];
+  console.log(windowWidth);
+  if (windowWidth < 768) return mobileContentArray.join('');
 
   return contentArray.join('');
 };

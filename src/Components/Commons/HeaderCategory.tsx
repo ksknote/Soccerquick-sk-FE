b3983126be688e899a41c9157c792d533f8bd63b
bible <mongry999@gmail.com>
2023-08-16 14:08:50 +0900

    [style]헤더 네비게이션 바 focus css 수정

--- src/Components/Commons/HeaderCategory.tsx
+++ src/Components/Commons/HeaderCategory.tsx
@@ -16,52 +16,63 @@ export default function HeaderCategory() {
   const isTeampage = location.pathname.includes('/teampage');
 
   return (
-    <StyledHeaderCategory>
-      <StyledCategoryText
-        focused={isHome}
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        홈
-      </StyledCategoryText>
+    <Wrapper>
+      <StyledHeaderCategory>
+        <StyledCategoryText
+          focused={isHome}
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          홈
+        </StyledCategoryText>
 
-      <StyledCategoryText
-        focused={isGroundPage}
-        onClick={() => {
-          navigate('/ground');
-        }}
-      >
-        경기장
-      </StyledCategoryText>
+        <StyledCategoryText
+          focused={isGroundPage}
+          onClick={() => {
+            navigate('/ground');
+          }}
+        >
+          경기장
+        </StyledCategoryText>
 
-      <StyledCategoryText
-        focused={isReviewPage}
-        onClick={() => {
-          navigate('/review');
-        }}
-      >
-        리뷰
-      </StyledCategoryText>
+        <StyledCategoryText
+          focused={isReviewPage}
+          onClick={() => {
+            navigate('/review');
+          }}
+        >
+          리뷰
+        </StyledCategoryText>
 
-      <StyledCategoryText
-        focused={isTeampage}
-        onClick={() => {
-          navigate('/teampage');
-        }}
-      >
-        팀
-      </StyledCategoryText>
-    </StyledHeaderCategory>
+        <StyledCategoryText
+          focused={isTeampage}
+          onClick={() => {
+            navigate('/teampage');
+          }}
+        >
+          팀
+        </StyledCategoryText>
+      </StyledHeaderCategory>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: none;
+  @media (min-width: 768px) {
+    display: block;
+    width: 100%;
+    background: white;
+    border-bottom: 1px solid rgb(238, 238, 238);
+  }
+`;
 
 const StyledHeaderCategory = styled.div`
   display: none;
   @media (min-width: 768px) {
     max-width: 120rem;
     padding: 0 2rem;
-    height: 3rem;
     margin: auto;
     display: flex;
     justify-content: flex-start;
@@ -69,24 +80,28 @@ const StyledHeaderCategory = styled.div`
   }
 
   @media (min-width: 1024px) {
-    padding: 3rem 2rem;
+    padding: 0rem 2rem;
   }
 `;
 
 const StyledCategoryText = styled.span<CategoryTextProps>`
+  height: 100%;
   padding: 0 1rem;
-  font-size: ${({ focused }) => (focused ? '1.9rem' : '1.6rem')};
+  font-size: 1.6rem;
   color: ${({ focused }) => (focused ? '#535353' : '#727f88')};
   font-weight: ${({ focused }) => (focused ? 'bold' : 'normal')};
+  border-bottom: ${({ focused }) => focused && '0.2rem solid #727f88'};
   cursor: pointer;
-
   &:hover {
     font-size: ${({ focused }) => (focused ? '2.2rem' : '2.1rem')};
     padding-bottom: 0.5rem;
     border-bottom: 2px solid #e8e8e8;
   }
+  @media (min-width: 768px) {
+    padding: 1rem 1rem;
+  }
   @media (min-width: 1024px) {
-    padding: 0 1.5rem;
-    font-size: ${({ focused }) => (focused ? '2.2rem' : '1.9rem')};
+    padding: 1rem 1.5rem;
+    font-size: 1.9rem;
   }
 `;

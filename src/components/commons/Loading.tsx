72134aa8d45c2fa86a226e65be58da3292edae3b
bible <mongry999@gmail.com>
2023-09-07 01:52:24 +0900

    feat: Loading 컴포넌트 분리

---
+++ src/components/commons/Loading.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import loadingGif from '../../../assets/icon/loading.gif';
+
+function Loading() {
+  return (
+    <Wrapper>
+      <img src={loadingGif} alt="loading" />
+    </Wrapper>
+  );
+}
+
+export default Loading;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 10rem;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 5rem;
+    height: 5rem;
+  }
+  @media (min-width: 1024px) {
+    height: 20rem;
+  }
+`;

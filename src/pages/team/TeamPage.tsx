887d6b1d92981a14c54a14cd0111492e6bdcb2e4
bible <mongry999@gmail.com>
2023-09-15 05:26:43 +0900

    feat: 변경된 team 게시글 작성 컴포넌트로 router path 변경

--- src/pages/team/TeamPage.tsx
+++ src/pages/team/TeamPage.tsx
@@ -3,16 +3,15 @@ import BaseLayout from '../../components/template/BaseLayout';
 import { Routes, Route } from 'react-router-dom';
 import FindingMember from '../../components/team/feed/TeamFeed';
 import ViewPage from '../../components/team/postDetail/TeamDetail';
-import SubmitPage from '../../components/team/writingForm/PostPage';
-import EditPage from '../../components/team/writingForm/EditPage';
+import WriteTeamPost from '../../components/team/writingForm/WriteTeamPost';
 import { BodyWrapper } from '../../styles/styled-components/CommonStyle';
 function TeamPage() {
   return (
     <BaseLayout>
       <BodyWrapper>
         <Routes>
-          <Route path="/submit" element={<SubmitPage />} />
-          <Route path="/edit/*" element={<EditPage />} />
+          <Route path="/submit" element={<WriteTeamPost />} />
+          <Route path="/edit/*" element={<WriteTeamPost />} />
           <Route path="/" element={<FindingMember />} />
           <Route path="/*" element={<ViewPage />} />
         </Routes>

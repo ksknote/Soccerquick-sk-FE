63b420a62f0ce00c439a696cf52c3750541df4ec
bible <mongry999@gmail.com>
2023-09-26 06:01:56 +0900

    chore: 캐러셀 컨텐츠 데이터 분리

--- src/components/main/Carousel.tsx
+++ src/components/main/Carousel.tsx
@@ -3,54 +3,9 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styled from 'styled-components';
-import image1 from '../../assets/image/carousel1.svg';
-import image2 from '../../assets/image/carousel2.gif';
-import image3 from '../../assets/image/carousel3.svg';
 import { useNavigate } from 'react-router-dom';
-const carouselContent = [
-  {
-    tag: '매월 업데이트',
-    title1: '한눈에 보는',
-    title2: '전국 풋살 지도.',
-    decription1: '싸커퀵 풋살 지도로',
-    description2: '풋살 경기장을 탐색해보세요!',
-    image: image1,
-    bgColor: '#0c0f22',
-    tagBgColor: '#16dcff',
-    tagColor: 'black',
-    titleColor: 'white',
-    decriptionColor: 'white',
-    link: '/ground',
-  },
-  {
-    tag: '팀원 모집/신청',
-    title1: '우리 동네',
-    title2: '풋살팀 만들기',
-    decription1: '싸커퀵에서 새로운 팀원을 모집해보세요!',
-    description2: '마음에 드는 팀이 있다면 함께해보세요!',
-    image: image2,
-    bgColor: '#B6F187',
-    tagBgColor: '#E3F9D1',
-    tagColor: '#054823',
-    titleColor: 'black',
-    decriptionColor: 'black',
-    link: '/teampage',
-  },
-  {
-    tag: '축구 커뮤니티',
-    title1: '어제 축구 보셨어요?',
-    title2: '',
-    decription1: '축구 마니아들을 위한 커뮤니티에서',
-    description2: '다양한 이야기를 공유해보세요!',
-    image: image3,
-    bgColor: '#DCD4F9',
-    tagBgColor: '#f3efff',
-    tagColor: '#473587',
-    titleColor: 'black',
-    decriptionColor: 'black',
-    link: '/community',
-  },
-];
+import { carouselContents } from './carouselContents';
+
 function Carousel() {
   const sliderRef = useRef(null);
   const navigate = useNavigate();
@@ -85,12 +40,12 @@ function Carousel() {
   return (
     <Wrapper>
       <Slider ref={sliderRef} {...settings}>
-        {carouselContent.map((slide, index) => (
+        {carouselContents.map((slide, index) => (
           <SlideBg key={slide.title1} bgColor={slide.bgColor}>
             <SliderContent>
               <IndexContainer>
                 <div onClick={previous}>❮</div>
-                {index}/{carouselContent.length}
+                {index}/{carouselContents.length}
                 <div onClick={next}>❯</div>
               </IndexContainer>
               <SliderText onClick={() => clickSlideHandler(slide)}>
---
+++ src/components/main/carouselContents.ts
@@ -0,0 +1,48 @@
+import image1 from '../../assets/image/carousel1.svg';
+import image2 from '../../assets/image/carousel2.gif';
+import image3 from '../../assets/image/carousel3.svg';
+
+export const carouselContents = [
+  {
+    tag: '매월 업데이트',
+    title1: '한눈에 보는',
+    title2: '전국 풋살 지도.',
+    decription1: '싸커퀵 풋살 지도로',
+    description2: '풋살 경기장을 탐색해보세요!',
+    image: image1,
+    bgColor: '#0c0f22',
+    tagBgColor: '#16dcff',
+    tagColor: 'black',
+    titleColor: 'white',
+    decriptionColor: 'white',
+    link: '/ground',
+  },
+  {
+    tag: '팀원 모집/신청',
+    title1: '우리 동네',
+    title2: '풋살팀 만들기',
+    decription1: '싸커퀵에서 새로운 팀원을 모집해보세요!',
+    description2: '마음에 드는 팀이 있다면 함께해보세요!',
+    image: image2,
+    bgColor: '#B6F187',
+    tagBgColor: '#E3F9D1',
+    tagColor: '#054823',
+    titleColor: 'black',
+    decriptionColor: 'black',
+    link: '/teampage',
+  },
+  {
+    tag: '축구 커뮤니티',
+    title1: '어제 축구 보셨어요?',
+    title2: '',
+    decription1: '축구 마니아들을 위한 커뮤니티에서',
+    description2: '다양한 이야기를 공유해보세요!',
+    image: image3,
+    bgColor: '#DCD4F9',
+    tagBgColor: '#f3efff',
+    tagColor: '#473587',
+    titleColor: 'black',
+    decriptionColor: 'black',
+    link: '/community',
+  },
+];
